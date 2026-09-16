51f18fdcab7190793b55bacff6e2151b903e1ca2
Rana Muhammad Rameez <30427743+ranamrameez@users.noreply.github.com>
2026-09-16 03:27:13 +0300

    fix: import bright card and topbar overrides

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './main/site.css'
 import './theme.css'
 import './brighter-theme.css'
+import './theme-bright-card-overrides.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
